502abbf1fbe375271232a2c1524e30ecb99ec696
danisuargarc <77943603+danisuargarc@users.noreply.github.com>
2021-02-15 11:41:09 -0500

    Add files via upload

---
+++ RibbonDemo/user-app/src/test/java/spring/ribbon/userapp/UserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.ribbon.userapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
